da2b1594cc94e18aeeee12a1a898fe9f6a514ce5
Mohammad Rafe <mrrafi5869@gmail.com>
2022-10-26 20:11:27 +0600

    premium data

--- index.js
+++ index.js
@@ -7,6 +7,8 @@ app.use(cors());
 
 const courses = require("./data/course.json")
 
+const premiumCourses = require('./data/Premium.json');
+
 app.get('/', (req, res) => {
     res.send("news api is running");
 })
@@ -19,6 +21,16 @@ app.get("/course/:id", (req, res) => {
     const id = req.params.id;
     const courseDetails = courses.find(course => course.id == id);
     res.send(courseDetails);
+})
+
+app.get('/premiumCourse',(req, res) => {
+    res.send(premiumCourses)
+})
+
+app.get('/premiumCourse/:id', (req, res) => {
+    const id = req.params.id;
+    const premiumSingleCourse = premiumCourses.find(premiumCourse => premiumCourse.id == id);
+    res.send(premiumSingleCourse);
 })
 
 app.listen(port, () => {
